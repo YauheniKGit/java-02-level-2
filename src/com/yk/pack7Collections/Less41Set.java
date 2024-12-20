899e6e8c2f9b02b8a4cfb0482e487b620c3cc27e
“Yauheni <yauheni.kastsiukou@gmail.com>
2024-12-20 11:49:16 +0300

    Add Lessons 41 - 42 Collections

---
+++ src/com/yk/pack7Collections/Less41Set.java
@@ -0,0 +1,20 @@
+package com.yk.pack7Collections;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Less41Set {
+
+    public static void main(String[] args) {
+        Set<String> set = new LinkedHashSet<>();
+        set.add("aaa");
+        set.add("aaa");
+        set.add("bbb");
+        set.add("ccc");
+
+        System.out.println(set);
+
+    }
+
+
+}
