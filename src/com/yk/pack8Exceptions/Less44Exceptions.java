304bad620d2cbb996176cb6ce16c5ea86cbad138
“Yauheni <yauheni.kastsiukou@gmail.com>
2024-12-21 12:31:36 +0300

    Change Lessons 44 Exception

--- src/com/yk/pack8Exceptions/Less44Exceptions.java
+++ src/com/yk/pack8Exceptions/Less44Exceptions.java
@@ -3,10 +3,22 @@ package com.yk.pack8Exceptions;
 public class Less44Exceptions {
 
     public static void main(String[] args) {
+        System.out.println("main start");
 
-        System.out.println("1111");
+            unsafe(-10);
+
+        System.out.println("main end");
+        //code
 
 
+    }
+
+    public static void unsafe(int value) throws IllegalArgumentException{
+        System.out.println("unsafe start");
+        if (value < 0) {
+            throw new IllegalArgumentException();
+        }
+        System.out.println("unsafe end");
     }
 
 
